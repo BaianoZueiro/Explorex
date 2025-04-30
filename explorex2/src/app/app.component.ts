04ae572a47af167715e08d1b3744df0f4cd9f8c9
Gabriel Azevedo <gabrielmazevedolima2016@gmail.com>
2025-04-30 10:48:27 -0300

    Header Feito

--- explorex2/src/app/app.component.ts
+++ explorex2/src/app/app.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { HeaderComponent } from './shared/components/header/header.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
+  imports: [HeaderComponent],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
